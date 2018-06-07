498949915e566ada6252be1c8163ef910e818794
d3m1d0v <y.a.demidov@yandex.ru>
2018-06-07 23:40:46 +0300

    Input file or dir path from console

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
-if (process.argv.length <= 2) {
-    console.log("Path to dir doesn't found in first command line argument\nExit");
-    process.exit(0);
-}
-
 const path = require('path');
 const fs = require('fs');
-
-let npath = path.normalize(process.argv[2]);
-if (!fs.existsSync(npath)) {
-    console.log("Error via reading the directory\nExit");
-    process.exit(0);
-}
-
-console.log("Listing directory (files only):");
-fs.readdirSync(npath)
-    .filter(item => fs.lstatSync(path.join(npath, item)).isFile())
-    .forEach(i => console.log("\t" + i));
-
 const express = require('express');
-const app = express();
 const PORT = 1337;
 
-app.use('/files', express.static(npath));
-app.listen(PORT, () => {
-    console.log('\nServer started on port ' + PORT);
+console.log("Please, enter path to directory or file:");
+process.stdin.addListener("data", function(data) {
+    try {
+        let fpath = data.toString().trim();
+        if (fpath === "") {
+            console.log("You didn't enter anything. Please, try again...");
+            return;
+        }
+        
+        fpath = path.normalize(fpath);
+        let stats = fs.lstatSync(fpath);
+        
+        if (stats.isDirectory()) {
+            console.log("This is directory.");
+        } else if (stats.isFile()) {
+            console.log("This is file.");
+        }
+
+        startServer(fpath);
+        process.stdin.destroy();
+    } catch(err) {
+        console.log("Error via reading dir or file. Please, try again...");
+    }
 });
+
+function startServer(path) {
+    express()
+        .use('/static', express.static(path))
+        .listen(PORT, () => console.log('Server started on port ' + PORT));
+}
